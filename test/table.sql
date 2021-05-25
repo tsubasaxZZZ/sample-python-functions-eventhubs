c35b8ee2e65b0f8937004923395446128604ea00
Tsubasa Nomura <tsubasa@nomupro.com>
2021-05-26 03:21:58 +0900

    Update

--- test/table.sql
+++ test/table.sql
@@ -10,6 +10,21 @@ CREATE TABLE fromfunctions (
     func_invocationid varchar(256)
 )
 
+CREATE TABLE fromstreamanalytics (
+    id varchar(256),
+    process_datetime datetime,
+    value_a real,
+    value_b real,
+    eh_enqueuedtimeutc datetime,
+    eh_offset varchar(1024),
+    eh_sequenceno bigint,
+    sql_insert_datetime datetime,
+    func_invocationid varchar(256),
+	EventProcessedUtcTime datetime,
+	EventEnqueuedUtcTime datetime,
+	PartitionId int
+)
+
 --チェック用--
 select count(*) from fromfunctions where id like 'test7_%' --order by eh_enqueuedtimeutc
 SELECT  * FROM [dbo].[fromfunctions] order by eh_enqueuedtimeutc desc
